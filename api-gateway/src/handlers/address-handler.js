43d685deaf9fdb57b7a9ea1a9b48cd24604b6d50
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2024-02-18 00:32:29 -0300

    fix: correcao de nome da classe

--- api-gateway/src/handlers/address-handler.js
+++ api-gateway/src/handlers/address-handler.js
@@ -1,20 +1,20 @@
 const axios = require("axios");
 
 class AddressHandler {
-  static async create(req, res) {
-    try {
-      const { data } = await axios.post(
-        `${process.env.ADDRESS_SERVICE_BASE_URL}/address`,
-        req.body
-      );
+    static async create(req, res) {
+        try {
+            const { data } = await axios.post(
+                `${process.env.ADDRESS_SERVICE_BASE_URL}/address`,
+                req.body
+            );
 
-      return res.status(200).json(data);
-    } catch (error) {
-      res.status(error?.response?.status || 500).json({
-        error: error?.response?.data?.error || "Server Error ",
-      });
+            return res.status(200).json(data);
+        } catch (error) {
+            res.status(error?.response?.status || 500).json({
+                error: error?.response?.data?.error || "Server Error ",
+            });
+        }
     }
-  }
 }
 
-module.exports = RegisterHandler;
+module.exports = AddressHandler;
