20d9cd9957e4e9d0085dcde314155235a2941f3f
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2024-02-18 01:13:30 -0300

    fix: passa a atualização do endereço para o service

--- address-service/src/controllers/address-ctrl.js
+++ address-service/src/controllers/address-ctrl.js
@@ -1,64 +1,44 @@
 const axios = require("axios");
-const AddressService = require("../services/address.service");
+const UserService = require("../services/user.service");
 
 class AddressController {
-  static async getByCEP(req, res) {
-    try {
-      const { cep, userId } = req.body;
+    static async getByCEP(req, res) {
+        try {
+            const { cep, userId } = req.body;
 
-      // Validação básica do CEP
-      if (!cep || cep.length !== 8 || !/^\d+$/.test(cep)) {
-        throw Exception("Invalid CEP", 400);
-      }
+            // Validação se o usuário está autenticado
+            const authUser = await UserService.userExistsById(id);
 
-      // Consulta a API Via CEP
-      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+            if (!authUser) {
+                throw Exception("User not found", 404);
+            }
 
-      // Verifica se o CEP é válido na API Via CEP
-      if (response.data.erro) {
-        throw Exception("CEP not found", 404);
-      }
+            // Validação básica do CEP
+            if (!cep || cep.length !== 8 || !/^\d+$/.test(cep)) {
+                throw Exception("Invalid CEP", 400);
+            }
 
-      // Cria ou atualiza o endereço no serviço
-      const addressData = {
-        street: response.data.logradouro,
-        neighborhood: response.data.bairro,
-        city: response.data.localidade,
-        state: response.data.uf,
-        country: response.data.pais || "Brasil",
-      };
+            // Consulta a API Via CEP
+            const response = await axios.get(
+                `https://viacep.com.br/ws/${cep}/json/`
+            );
 
-      const user = await AddressService.getUserById(userId);
-      if (user === null) {
-        throw Exception("User not found", 404);
-      }
+            // Verifica se o CEP é válido na API Via CEP
+            if (response.data.erro) {
+                throw Exception("CEP not found", 404);
+            }
 
-      user.street = addressData.street;
-      user.neighborhood = addressData.neighborhood;
-      user.city = addressData.city;
-      user.state = addressData.state;
-      user.country = addressData.country;
-      user.cep = cep;
-      await user.save();
+            //Atualiza o endereço do usuário
+            const updatedUser = await UserService.updateById(
+                authUser._id,
+                response.data
+            );
 
-      const userResponse = {
-        name: user.name,
-        email: user.email,
-        cpf: user.cpf,
-        street: user.street,
-        number: user.number,
-        neighborhood: user.neighborhood,
-        city: user.city,
-        state: user.state,
-        country: user.country,
-        cep: user.cep,
-      };
-
-      res.status(200).json(userResponse);
-    } catch (error) {
-      console.log(error);
-      res.status(error.status || 500).json({ message: error.message });
+            res.status(200).json(updatedUser);
+        } catch (error) {
+            console.log(error);
+            res.status(error.status || 500).json({ message: error.message });
+        }
     }
-  }
 }
 module.exports = AddressController;
